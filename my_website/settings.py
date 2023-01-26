e9b6fc81a30c0a57b52571724ad643feeaa61580
Abdul Karim Zid <zid.abdulkarim@gmail.com>
2023-01-26 10:25:02 +0100

    Update settings.py

--- my_website/settings.py
+++ my_website/settings.py
@@ -129,10 +129,3 @@ MEDIA_ROOT = BASE_DIR / "media"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-EMAIL_HOST = 'smtp.zoho.com'
-EMAIL_HOST_USER = '############'
-EMAIL_HOST_PASSWORD = '##########'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
